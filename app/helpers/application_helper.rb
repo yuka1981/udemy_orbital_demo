ecdbe3a2585b9e6a64a24c7d789e2aee1cf4fa9f
Reid <reid.lin@nextlink.com.tw>
2021-11-23 23:08:37 +0800

    Add custom view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def full_title(page_title = ' ')
+    base_title = 'Orbit Feedback Application'
+    puts "page_title: #{page_title.empty?}"
+
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
