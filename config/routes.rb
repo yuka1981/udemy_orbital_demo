2bfaaf736abcaa1fa164bdaa296dc7a6ef0983cb
Reid <touyalin@gmail.com>
2021-11-25 01:29:31 +0800

    complete signup function

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/contact', to: 'html_pages#contact'
 
   get '/signup', to: 'users#new'
+  resources :users
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid_email',
+                                         password: 'password',
+                                         password_confirmation: 'pass' } }
+    end
+
+    assert_template 'users/new'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Reid Lin',
+                                         email: 'reidlin@example.com',
+                                         password: 'password123',
+                                         password_confirmation: 'password123' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
