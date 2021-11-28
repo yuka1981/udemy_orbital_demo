be43da995d6df4615b509202d02a083be5bc76ff
Reid <touyalin@gmail.com>
2021-11-28 10:02:10 +0800

    add snapshot model and run migration

---
+++ db/migrate/20211128015754_create_snapshots.rb
@@ -0,0 +1,12 @@
+class CreateSnapshots < ActiveRecord::Migration[6.0]
+  def change
+    create_table :snapshots do |t|
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :snapshots, %i[user_id created_at]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_161835) do
+ActiveRecord::Schema.define(version: 2021_11_28_015754) do
+
+  create_table "snapshots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_snapshots_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_snapshots_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_11_27_161835) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "snapshots", "users"
 end
